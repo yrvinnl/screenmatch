5c33c0656caebece8f1310ee2e5a5b56cf045928
Yrvin Narro <yrvinnl@gmail.com>
2024-10-20 17:24:13 -0500

    Primer commit de screen

---
+++ src/Principal.java
@@ -0,0 +1,32 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDeLasEvualuaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dargon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+//        System.out.println(cadaDragon.ge);
+
+
+
+    }
+}
---
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Pelicula extends Titulo {
+    private String director;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
